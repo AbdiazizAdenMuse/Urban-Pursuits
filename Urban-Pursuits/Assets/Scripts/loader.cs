ba845ab95ed503b4658a8461f07786f1a7bf243b
AbdiazizAdenMuse <162517966+AbdiazizAdenMuse@users.noreply.github.com>
2024-03-19 16:46:39 -0300

    intro

---
+++ Urban-Pursuits/Assets/Scripts/loader.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class loader : MonoBehaviour
+{
+    [SerializeField] GameObject loadingScreen;
+    [SerializeField] Slider slider;
+    [SerializeField] Text progressText;
+
+    private float timeDelay = 8f;
+    private float timeElapsed;
+    private void Update()
+    {
+        timeElapsed += Time.deltaTime;
+       // loadingScreen.SetActive(true);
+
+        if (timeElapsed > timeDelay)
+        {
+            
+            StartCoroutine(loadAsynchronously(1));
+        }
+    }
+    public void loadScene(int sceneIndex)
+    {
+
+
+    }
+
+    IEnumerator loadAsynchronously(int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+            slider.value = progress;
+            progressText.text = progress * 100f + "%";
+            yield return null;
+        }
+    }
+}
